8e58192a09ccc18cc3c0d77d09319265975dd876
vasundharabala <89012316+vasundharabala@users.noreply.github.com>
2022-11-10 07:31:43 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,6 @@
+var data = 0;
+  document.getElementById("counting").innerText = data;
+  function increment() {
+      data = data + 1;
+      document.getElementById("counting").innerText = data;
+  }
